5a3588315c4249fd15a1c56ebe6a3089669aa5be
David Ranoia <105903983+DjTuner13@users.noreply.github.com>
2024-06-28 10:53:33 -0400

    Added functionality to movie rail
    Updated:
    - movie rail to grab 20 movies
    - movie rail to be scrollable
    - movie rail to enlarge with hover and show text
    - updated css to display better centering
    - updated movie to link user to movie html page
    - update movie page to allow users to watch the trailer
    - added a shader box to movie page

--- simpleSite/jsScripts/fetchMoviePoster.js
+++ simpleSite/jsScripts/fetchMoviePoster.js
@@ -1,28 +1,81 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const apiUrl = `https://api.themoviedb.org/3/tv/94997?api_key=${config.API_KEY}`;
+    const urlParams = new URLSearchParams(window.location.search);
+    const movieId = urlParams.get('id');
+    const movieDetailsApiUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${config.API_KEY}`;
+    const trailerApiUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos`;
 
-    fetch(apiUrl)
+    fetch(movieDetailsApiUrl, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${config.BEARER_TOKEN}`,
+            'accept': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const posterPath = data.poster_path;
+        const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+        const originalTitle = data.original_title;
+        const overview = data.overview;
+        const releaseDate = data.release_date;
+        const genres = data.genres.map(genre => genre.name).join(', ');
+
+        document.getElementById("movie-title").textContent = originalTitle;
+        const moviePoster = document.getElementById("movie-poster");
+        moviePoster.src = posterUrl;
+        moviePoster.alt = originalTitle;
+        
+        // Delay revealing the image to prevent flicker
+        setTimeout(() => {
+            moviePoster.classList.remove('hidden');
+        }, 2000);
+
+        document.getElementById("movie-overview").textContent = `Overview: ${overview}`;
+        document.getElementById("movie-release-date").textContent = `Release Date: ${releaseDate}`;
+        document.getElementById("movie-genres").textContent = `Genres: ${genres}`;
+
+        fetchTrailer(trailerApiUrl);
+    })
+    .catch(error => {
+        console.error("Error fetching the movie details:", error);
+    });
+
+    function fetchTrailer(apiUrl) {
+        fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${config.BEARER_TOKEN}`,
+                'accept': 'application/json'
+            }
+        })
         .then(response => response.json())
         .then(data => {
-            // Extract data from the response
-            const posterPath = data.poster_path;
-            const originalTitle = data.original_title;
-            const overview = data.overview;
-            const releaseDate = data.release_date;
-            const genres = data.genres.map(genre => genre.name).join(', ');
+            const trailers = data.results.filter(video => video.type === 'Trailer' && video.site === 'YouTube');
+            if (trailers.length > 0) {
+                const trailer = trailers[0];
+                const trailerUrl = `https://www.youtube.com/embed/${trailer.key}?autoplay=1&origin=https%3A%2F%2Fwww.themoviedb.org&hl=en&modestbranding=1&fs=1&autohide=1`;
+                const trailerFrame = document.getElementById("trailer-frame");
 
-            // Construct the full URL for the poster image
-            const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+                const trailerButton = document.getElementById("movie-trailer");
+                trailerButton.addEventListener("click", function() {
+                    const modal = document.getElementById("trailer-modal");
+                    modal.style.display = "flex";
+                    trailerFrame.src = trailerUrl; // Set src to include autoplay
+                });
 
-            // Set the movie details in the DOM
-            document.getElementById("movie-title").textContent = originalTitle;
-            document.getElementById("movie-poster").src = posterUrl;
-            document.getElementById("movie-overview").textContent = `Overview: ${overview}`;
-            document.getElementById("movie-release-date").textContent = `Release Date: ${releaseDate}`;
-            document.getElementById("movie-genres").textContent = `Genres: ${genres}`;
+                const modalBackground = document.getElementById("modal-background");
+                modalBackground.addEventListener("click", function() {
+                    const modal = document.getElementById("trailer-modal");
+                    modal.style.display = "none";
+                    trailerFrame.src = ""; // Stop the video and reset src
+                });
+            } else {
+                document.getElementById("trailer-container").style.display = 'none';
+            }
         })
         .catch(error => {
-            console.error("Error fetching the movie data:", error);
+            console.error("Error fetching the trailer:", error);
         });
+    }
 });
 
--- simpleSite/jsScripts/fetchMovies.js
+++ simpleSite/jsScripts/fetchMovies.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const topRatedMoviesApiUrl = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200';
     const authHeader = `Bearer ${config.BEARER_TOKEN}`;
 
+    fetchMovies(popularMoviesApiUrl, 'popular-movies-container');
+    fetchMovies(topRatedMoviesApiUrl, 'top-rated-movies-container');
 
     function fetchMovies(apiUrl, containerId) {
         fetch(apiUrl, {
@@ -14,8 +16,9 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .then(response => response.json())
         .then(data => {
-            const movies = data.results.slice(0, 10);
+            const movies = data.results.slice(0, 20);
             const container = document.getElementById(containerId);
+            container.innerHTML = ''; // Clear the container
 
             movies.forEach(movie => {
                 const movieElement = document.createElement('div');
@@ -25,19 +28,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
 
                 movieElement.innerHTML = `
-                    <img src="${posterUrl}" alt="${movie.title}">
+                    <img src="${posterUrl}" alt="${movie.title}" data-id="${movie.id}">
                     <div class="title">${movie.title}</div>
                 `;
+
+                movieElement.querySelector('img').addEventListener('click', function() {
+                    const movieId = this.getAttribute('data-id');
+                    window.location.href = `movie.html?id=${movieId}`;
+                });
 
                 container.appendChild(movieElement);
             });
         })
         .catch(error => {
-            console.error(`Error fetching the movies from ${apiUrl}:`, error);
+            console.error(`Error fetching the movies: ${error}`);
         });
     }
-
-    fetchMovies(popularMoviesApiUrl, 'popular-movies-container');
-    fetchMovies(topRatedMoviesApiUrl, 'top-rated-movies-container');
 });
 
